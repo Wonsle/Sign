0eeebc72d95144eed46f9eb4e491881ba2b3a9b8
昆霖 李 <kerberos8280@gmail.com>
2021-01-13 20:52:59 +0800

    加入專案檔案。

---
+++ formSign.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Sign
+{
+    partial class formSing
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancle = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSave.Location = new System.Drawing.Point(627, 140);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(103, 58);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "儲存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancle
+            // 
+            this.btnCancle.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnCancle.Location = new System.Drawing.Point(627, 219);
+            this.btnCancle.Name = "btnCancle";
+            this.btnCancle.Size = new System.Drawing.Size(103, 58);
+            this.btnCancle.TabIndex = 2;
+            this.btnCancle.Text = "清除";
+            this.btnCancle.UseVisualStyleBackColor = true;
+            this.btnCancle.Click += new System.EventHandler(this.btnCancle_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(31, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(590, 375);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "簽名";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(7, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(577, 342);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // formSing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCancle);
+            this.Controls.Add(this.btnSave);
+            this.Name = "formSing";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.formSing_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ formSign.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Sign
+{
+    public partial class formSing : Form
+    {
+        private GraphicsPath mousePath = new GraphicsPath();
+        private int myAlpha = 100;
+        private Color myUserColor = new Color();
+        private int myPenWidth = 3;
+        public Bitmap savedBitmap;
+        public formSing()
+        {
+            InitializeComponent();
+        }
+
+        private void formSing_Load(object sender, EventArgs e)
+        {
+            this.Text = "簽名測試";
+            this.pictureBox1.BackColor = Color.White;
+        }
+
+        /// <summary>
+        /// 開始簽名
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mousePath.StartFigure();
+            }
+        }
+
+        /// <summary>
+        /// 移動畫筆
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                try
+                {
+                    mousePath.AddLine(e.X, e.Y, e.X, e.Y);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            //重新繪製控制項
+            pictureBox1.Invalidate();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                myUserColor = Color.Black;
+                myAlpha = 255;
+                Pen p = new Pen(Color.FromArgb(myAlpha, myUserColor), myPenWidth);
+                e.Graphics.DrawPath(p, mousePath);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            savedBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(savedBitmap, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+            savedBitmap.Save($"{Application.StartupPath}\\Sign.png", ImageFormat.Png);
+
+
+            //Bitmap bmp = savedBitmap;
+            //BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+            //int length = data.Stride * data.Height;
+            //IntPtr prt = data.Scan0;
+            //byte[] buff = new byte[length];
+            //Marshal.Copy(prt, buff, 0, length);
+            //for (int i = 3; i < length; i += 4)
+            //{
+            //    if (buff[i - 1] >= 230 && buff[i - 2] >= 230 && buff[i - 3] >= 230)
+            //    {
+            //        buff[i] = 0;
+            //    }
+            //}
+            //Marshal.Copy(buff, 0, prt, length);
+            //bmp.UnlockBits(data);
+            //bmp.Save($"{Application.StartupPath}\\Sign.png", ImageFormat.Png);
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            pictureBox1.CreateGraphics().Clear(Color.White);
+            mousePath.Reset();
+        }
+    }
+}
